0c5557f96dab0efe35ae0bf0f6bc33d7b0e9b978
RyanYou <13763387287@139.com>
2016-03-17 10:44:43 +0800

    add one module in test branch ..

---
+++ demolib/src/main/java/com/example/BlockingQueueDemo.java
@@ -0,0 +1,12 @@
+package com.example;
+
+/**
+ * Created by Ryan.You on 2016/3/17.
+ */
+public class BlockingQueueDemo {
+
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+
+}
